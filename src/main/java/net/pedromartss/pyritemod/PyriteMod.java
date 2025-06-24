9ee8850f01f42736c004fd6dc5e29b439f543f27
pedr0martss <121310953+pedr0martss@users.noreply.github.com>
2025-06-24 18:02:21 -0300

    Modificaçoes Importantes

---
+++ src/main/java/net/pedromartss/pyritemod/PyriteMod.java
@@ -0,0 +1,97 @@
+package net.pedromartss.pyritemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.pedromartss.pyritemod.block.ModBlocks;
+import net.pedromartss.pyritemod.component.ModDataComponentTypes;
+import net.pedromartss.pyritemod.effect.ModEffects;
+import net.pedromartss.pyritemod.enchantment.ModEnchantmentEffects;
+import net.pedromartss.pyritemod.item.ModCreativeModeTabs;
+import net.pedromartss.pyritemod.item.ModItems;
+import net.pedromartss.pyritemod.potion.ModPotions;
+import net.pedromartss.pyritemod.sound.ModSounds;
+import net.pedromartss.pyritemod.util.ModItemProperties;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(PyriteMod.MOD_ID)
+public class PyriteMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "pyritemod";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public PyriteMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        ModDataComponentTypes.register(modEventBus);
+        ModSounds.register(modEventBus);
+
+        ModEffects.register(modEventBus);
+        ModPotions.register(modEventBus);
+
+        ModEnchantmentEffects.register(modEventBus);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            ComposterBlock.COMPOSTABLES.put(ModItems.PEACH.get(), 0.4f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.PEACH_SEEDS.get(), 0.15f);
+        });
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.PYRITE);
+            event.accept(ModItems.RAW_PYRITE);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.PYRITE_BLOCK);
+            event.accept(ModBlocks.RAW_PYRITE_BLOCK);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            ModItemProperties.addCustomItemProperties();
+
+        }
+    }
+}
